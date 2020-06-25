53d7fbb6d70bcdc0b003a277b2996de207af8d3f
hieunugent <hieunugent@gmail.com>
2020-06-24 21:50:25 -0700

    rearange the folder and add backend

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import Main from "./component/navigation/main";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+        <p className="App-intro">;{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
